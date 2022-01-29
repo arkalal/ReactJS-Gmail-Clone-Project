625e861e916d0133a59c2c007bd2240261b7fa53
arkalal <arkalal.123@gmail.com>
2022-01-29 12:02:44 +0530

    Email Body is ready

--- src/Email body/EmailList.js
+++ src/Email body/EmailList.js
@@ -8,7 +8,7 @@ const EmailList = () => {
     return <div className='emailList'>
         <EmailListSettings></EmailListSettings>
         <EmailType></EmailType>
-        <EmailBody name='Arkalal' message='This is just a test' time='2:30 PM' subject='Test Subject'></EmailBody>
+        <EmailBody name='Arkalal' message='This is just a test hagsgasgdas I am something that cannnot be changes beacacahcahcjha of the aro kichu likhte obe beacuse na hole eta hobe nah' time='2:30 PM' subject='Test Subject'></EmailBody>
     </div>;
 };
 
